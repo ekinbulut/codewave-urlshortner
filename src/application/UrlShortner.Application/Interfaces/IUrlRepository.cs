a993846cbef7060d260a8b824a41e6f234156172
Ekin Bulut <ekinbulut@gmail.com>
2024-03-05 11:58:17 +0300

    redis service implementation

--- src/application/UrlShortner.Application/Interfaces/IUrlRepository.cs
+++ src/application/UrlShortner.Application/Interfaces/IUrlRepository.cs
@@ -2,20 +2,9 @@ using UrlShortner.Domain.Entities;
 
 namespace UrlShortner.Application.Interfaces;
 
-public class IUrlRepository
+public interface IUrlRepository
 {
-    public async Task<Url> GetByUrlAsync(string longUrl)
-    {
-        throw new NotImplementedException();
-    }
-
-    public async Task AddAsync(Url url)
-    {
-        throw new NotImplementedException();
-    }
-
-    public async Task<Url> GetByShortUrlAsync(string shortUrl)
-    {
-        throw new NotImplementedException();
-    }
+   Task<Url?> GetByUrlAsync(string longUrl);
+   Task AddAsync(Url url);
+   Task<Url> GetByShortUrlAsync(string shortUrl);
 }
--- src/application/UrlShortner.Application/Repositories/UrlRepository.cs
+++ src/application/UrlShortner.Application/Repositories/UrlRepository.cs
@@ -1,8 +1,42 @@
+using Newtonsoft.Json;
 using UrlShortner.Application.Interfaces;
+using UrlShortner.Domain.Entities;
+using Urlshortner.Infrastructure.Services;
 
 namespace UrlShortner.Application.Repositories;
 
 public class UrlRepository : IUrlRepository
 {
-    
+    private IRedisService _redisService;
+
+    public UrlRepository(IRedisService redisService)
+    {
+        _redisService = redisService;
+    }
+
+    public async Task<Url?> GetByUrlAsync(string longUrl)
+    {
+        var db = _redisService.GetDb();
+        var value = db.StringGet(longUrl);
+
+        if (value.HasValue)
+        {
+            var urlString = (string)value;
+            return JsonConvert.DeserializeObject<Url>(urlString);
+        }
+
+        return null;
+    }
+
+    public async Task AddAsync(Url url)
+    {
+        var db = _redisService.GetDb();
+        var value = JsonConvert.SerializeObject(url, Formatting.Indented);
+        db.StringSet(url.LongUrl, value);
+    }
+
+    public async Task<Url> GetByShortUrlAsync(string shortUrl)
+    {
+        throw new NotImplementedException();
+    }
 }
